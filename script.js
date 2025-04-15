a6bdb1869ac661e1afd75d01b6750ee7ba7c381d
Michal <m.krepiniewicz@gmail.com>
2025-04-15 20:09:24 +0200

    Using free open API for maps in Js: Leaflet

--- script.js
+++ script.js
@@ -17,7 +17,17 @@ navigator.geolocation.getCurrentPosition(
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude]; // its setting the localisation parameters
+    const map = L.map('map').setView(coords, 13); // 13 is representing zoom it and out option
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map); // addto is tageting specific div element from html by id
+    const marker = L.marker(coords).addTo(map).bindPopup('A pretty CSS3 popup'); // allows us to add and customize marker
   },
+
   function () {
     alert('Could not get your position.');
   }
